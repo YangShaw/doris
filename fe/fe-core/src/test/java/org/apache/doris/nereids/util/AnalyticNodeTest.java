53fe5382973b48c2e273ff4fd9345fff66f31b3d
fuyu29 <fuyu29@meituan.com>
2022-11-22 22:29:31 +0800

    function registry for window functions

--- fe/fe-core/src/test/java/org/apache/doris/nereids/util/AnalyticNodeTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/util/AnalyticNodeTest.java
@@ -56,9 +56,13 @@ public class AnalyticNodeTest extends TestWithFeService {
     @Test
     public void testAnalyze() {
         String sql = "SELECT sum(s_suppkey) OVER(PARTITION BY s_nation ORDER BY s_name) FROM supplier";
+        PlanChecker.from(connectContext).checkPlannerResult(sql);
+    }
 
-        String sql2 = "SELECT sum(s_suppkey) OVER(PARTITION BY s_nation ORDER BY s_name ROWS BETWEEN 1 preceding AND current row) FROM supplier";
-        PlanChecker.from(connectContext).checkPlannerResult(sql2);
+    @Test
+    public void testToString() {
+        String sql = "SELECT sum(s_suppkey) OVER(PARTITION BY s_nation ORDER BY s_name ROWS BETWEEN 1 preceding AND current row) FROM supplier";
+        PlanChecker.from(connectContext).checkPlannerResult(sql);
     }
 
     @Test
