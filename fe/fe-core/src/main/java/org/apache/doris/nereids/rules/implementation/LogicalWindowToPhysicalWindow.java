7e79b711b21855843b1c33659916c076847d4628
fuyu29 <fuyu29@meituan.com>
2023-01-30 20:32:20 +0800

    fix requiredProperties of PhysicalWindow

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/implementation/LogicalWindowToPhysicalWindow.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/implementation/LogicalWindowToPhysicalWindow.java
@@ -164,7 +164,7 @@ public class LogicalWindowToPhysicalWindow extends OneImplementationRuleFactory 
                 root);
         // todo: add isAnalyticSort to physicalWindow
         if (windowFrameGroup.partitionKeyList.isEmpty() && requiredOrderKeys.isEmpty()) {
-            return physicalWindow.withRequirePropertiesAndChild(RequireProperties.of(), root);
+            return physicalWindow.withRequirePropertiesAndChild(RequireProperties.followParent(), root);
         }
 
         PhysicalProperties properties;
